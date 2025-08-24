9ed06928596ee569ed2917f37e55bf5353427a2f
walfrus <walfywalrus@gmail.com>
2025-08-23 21:30:52 -0700

    :(

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,4 @@
-export default async function handler(req, res) {
+// api/ping.js
+export default function handler(req, res) {
   res.status(200).json({ ok: true, time: new Date().toISOString() });
 }
